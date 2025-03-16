8b9764d9f0913a566911e27d337ad237387ae591
huongnv04 <169079209+huongnv04@users.noreply.github.com>
2025-03-16 12:21:33 +0700

    login

---
+++ BTL_GK_Finish/BTL_GK/BTL_GK/Program.cs
@@ -0,0 +1,26 @@
+﻿using BTL_GK.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL_GK
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+           Application.Run(new Login());
+            // Admin adminForm = new Admin(1); // Truyền vào role Admin (1)
+            //Application.Run(adminForm); // Chạy Admin Form
+          //Application.Run(new NhanVien());
+        }
+    }
+}
